e083bb4304c73130748eb794c19fb13066dd1c28
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2020-04-22 17:55:44 +0300

    Done with Composite

---
+++ src/Composite/HeadFirst/CompositeIterator.java
@@ -0,0 +1,40 @@
+package Composite.HeadFirst;
+
+import java.util.Iterator;
+import java.util.Stack;
+
+class CompositeIterator implements java.util.Iterator<MenuComponent> {
+    Stack<Iterator<MenuComponent>> stack = new Stack<>();
+
+    public CompositeIterator(Iterator<MenuComponent> iterator) {
+        stack.push(iterator);
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (stack.empty()) {
+            return false;
+        } else {
+            Iterator<MenuComponent> iterator = stack.peek();
+            if (!iterator.hasNext()) {
+                stack.pop();
+                return hasNext();
+            } else {
+                return true;
+            }
+        }
+    }
+
+    @Override
+    public MenuComponent next() {
+        if (hasNext()) {
+            Iterator<MenuComponent> iterator = stack.peek();
+            MenuComponent component = iterator.next();
+
+            stack.push(component.iterator());
+
+            return component;
+        }
+        return null;
+    }
+}
